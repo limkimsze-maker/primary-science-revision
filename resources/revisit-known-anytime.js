072b74b28a42274b61b9abb5cce97d43e9ce9bfa
limkimsze-maker <lim_kim_sze@moe.edu.sg>
2026-09-08 10:37:36 +0800

    Bump known-review reload to latest build

--- resources/revisit-known-anytime.js
+++ resources/revisit-known-anytime.js
@@ -1,5 +1,5 @@
 (()=>{
-const VERSION='20260908d';
+const VERSION='20260908e';
 const TARGET_KEY='psleScience_revisit_known_target_v1';
 const DRAFT_KEY='psleScience_revisit_drafts_v1';
 let tries=0,observer=null;
