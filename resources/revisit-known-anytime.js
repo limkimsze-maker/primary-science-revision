c4be9bc20014e3aef2d6a1d732ea78a1c6c597b0
limkimsze-maker <lim_kim_sze@moe.edu.sg>
2026-09-08 09:54:45 +0800

    Keep known review reload on latest UI build

--- resources/revisit-known-anytime.js
+++ resources/revisit-known-anytime.js
@@ -1,5 +1,5 @@
 (()=>{
-const VERSION='20260908b';
+const VERSION='20260908c';
 const TARGET_KEY='psleScience_revisit_known_target_v1';
 const DRAFT_KEY='psleScience_revisit_drafts_v1';
 let tries=0,observer=null;
