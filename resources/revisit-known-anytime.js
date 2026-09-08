4c844d33c5bf7b73c8f4c69ff23fe49efaca0b0d
limkimsze-maker <lim_kim_sze@moe.edu.sg>
2026-09-08 11:20:05 +0800

    Keep revisit reloads on latest trainer build

--- resources/revisit-known-anytime.js
+++ resources/revisit-known-anytime.js
@@ -1,5 +1,5 @@
 (()=>{
-const VERSION='20260908g';
+const VERSION='20260908h';
 const TARGET_KEY='psleScience_revisit_known_target_v1';
 const DRAFT_KEY='psleScience_revisit_drafts_v1';
 let tries=0,observer=null;
